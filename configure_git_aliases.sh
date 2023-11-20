09882f71b95e901f7669fec247a74ea499f56708
INatsukiI <e1q20019@oit.ac.jp>
2023-11-20 23:55:54 +0900

    first commit

---
+++ configure_git_aliases.sh
@@ -0,0 +1,6 @@
+git config --global alias.ci commit
+git config --global alias.br branch
+git config --global alias.co checkout
+git config --global alias.cob "checkout -b"
+git config --global alias.cp cherry-pick
+git config --global alias.st status
